c905f5c4e26f52e710197590526850f9d592acc3
suzuki-mar <supasu145@gmail.com>
2022-03-20 14:54:00 +0900

    Number Displayの実装完了

--- src/github.com/suzuki-mar/poker/number.go
+++ src/github.com/suzuki-mar/poker/number.go
@@ -1,9 +1,25 @@
 package poker
 
+import (
+	"strconv"
+)
+
 type Number struct {
 	value int
 }
 
 func (n Number) DisplayString() string {
-	return "aaa"
+
+	switch n.value {
+	case 1:
+		return "A"
+	case 11:
+		return "J"
+	case 12:
+		return "Q"
+	case 13:
+		return "K"
+	default:
+		return strconv.Itoa(n.value)
+	}
 }
--- src/github.com/suzuki-mar/poker/number_test.go
+++ src/github.com/suzuki-mar/poker/number_test.go
@@ -1,13 +1,37 @@
 package poker
 
 import (
-	"fmt"
+	"strconv"
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	num := number{}
-	num.value = 2
+//arr := [...] string{"Golang", "Java"}
 
-	fmt.Println(num.displayString())
+func TestDisplay(t *testing.T) {
+	tests := []struct {
+		number   Number
+		expected string
+	}{
+		{number: Number{1}, expected: "A"},
+		{number: Number{2}, expected: "2"},
+		{number: Number{10}, expected: "10"},
+		{number: Number{11}, expected: "J"},
+		{number: Number{12}, expected: "Q"},
+		{number: Number{13}, expected: "K"},
+	}
+	for _, tt := range tests {
+		number := tt.number
+		value := strconv.Itoa(number.value)
+
+		t.Run(value, func(t *testing.T) {
+			// t.Fatalf でテストが失敗した場合でもクリーンアップ処理は呼び出される
+			t.Cleanup(func() {
+				t.Log("cleanup!")
+			})
+
+			if number.DisplayString() != tt.expected {
+				t.Errorf("期待した値 %v 実際の値 %v", tt.expected, number.DisplayString())
+			}
+		})
+	}
 }
